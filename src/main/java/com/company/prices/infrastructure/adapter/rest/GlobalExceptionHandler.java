49be2bf24ae75073314adb7b31f9f0fa325681c7
Carlos Saltrón <csaltron@gmail.com>
2025-12-26 19:11:14 -0300

    Mejora en GlobalExceptionHandler para simplificar el código mediante el uso de record. Se actualiza README

--- src/main/java/com/company/prices/infrastructure/adapter/rest/GlobalExceptionHandler.java
+++ src/main/java/com/company/prices/infrastructure/adapter/rest/GlobalExceptionHandler.java
@@ -71,27 +71,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    public static class ErrorResponse {
-        private final int status;
-        private final String message;
-        private final LocalDateTime timestamp;
-
-        public ErrorResponse(int status, String message, LocalDateTime timestamp) {
-            this.status = status;
-            this.message = message;
-            this.timestamp = timestamp;
-        }
-
-        public int getStatus() {
-            return status;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public LocalDateTime getTimestamp() {
-            return timestamp;
-        }
+    public record ErrorResponse(int status, String message, LocalDateTime timestamp) {
     }
 }
