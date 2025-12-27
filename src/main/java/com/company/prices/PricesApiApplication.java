152c72ad8930918047400690fd1a4f3d8cf23fb5
Carlos Saltrón <csaltron@gmail.com>
2025-12-27 13:07:26 -0300

    refactor: reorganiza clase bootstrap

---
+++ src/main/java/com/company/prices/PricesApiApplication.java
@@ -0,0 +1,11 @@
+package com.company.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PricesApiApplication.class, args);
+    }
+}
--- src/test/java/com/company/prices/integration/PriceControllerIntegrationTest.java
+++ src/test/java/com/company/prices/integration/PriceControllerIntegrationTest.java
@@ -1,6 +1,6 @@
 package com.company.prices.integration;
 
-import com.company.prices.infrastructure.config.PricesApiApplication;
+import com.company.prices.PricesApiApplication;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
